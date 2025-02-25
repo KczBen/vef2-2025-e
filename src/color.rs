546bd182cfa97f5a384f6e69fc5f6b32090f1ec0
Bence Koczogh <koczogh.b@gmail.com>
2025-02-25 10:53:02 +0000

    More SIMD colour writing

--- src/color.rs
+++ src/color.rs
@@ -1,5 +1,5 @@
 #![allow(unused_unsafe)]
-use std::arch::wasm32::{f32x4_extract_lane, f32x4_max, f32x4_min, f32x4_mul, f32x4_splat, f32x4_sqrt};
+use std::arch::wasm32::{f32x4_extract_lane, f32x4_max, f32x4_min, f32x4_mul, f32x4_nearest, f32x4_splat, f32x4_sqrt, u8x16_extract_lane};
 
 use crate::vector3::Vector3;
 
@@ -36,7 +36,11 @@ pub fn write_color(pixel_color: Vector3, texture: &mut Vec<u8>, pixel_index: usi
 
     let clamped = unsafe { f32x4_min(f32x4_max(rgba_gamma, lower_bound), upper_bound) };
 
-    let color = unsafe { f32x4_mul(f32x4_splat(255.999), clamped) };
+    // Experimental: Pack more colours in one vector
+    // u8x16 should fit 4 pixels as RGBA, and only use SIMD operations
+    let color = unsafe { f32x4_nearest(f32x4_mul(f32x4_splat(255.999), clamped)) };
+
+    println!("u8 lane 0 is {}", unsafe { u8x16_extract_lane::<0>(color) });
 
 
     texture[pixel_index] = unsafe { f32x4_extract_lane::<0>(color) } as u8;
