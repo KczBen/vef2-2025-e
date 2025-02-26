0e83cf6db0519f730cf0d854a56ca0d835cdfcde
Bence Koczogh <koczogh.b@gmail.com>
2025-02-26 17:09:35 +0000

    Remove scalar colour writing code
    SIMD path is measurably faster, no need for old path

--- src/color.rs
+++ src/color.rs
@@ -16,22 +16,7 @@ fn linear_to_gamma(linear_component: f32) -> f32 {
     return 0.0;
 }
 
-#[inline(never)]
 pub fn write_color(pixel_color: Vector3, texture: &mut Vec<u8>, pixel_index: usize) {
-    /*let r = linear_to_gamma(pixel_color.x());
-    let g = linear_to_gamma(pixel_color.y());
-    let b = linear_to_gamma(pixel_color.z());
-
-    let intensity = Interval::new(0.0, 0.999);
-
-    let rbyte = (255.999 * intensity.clamp(r)) as u32;
-    let gbyte = (255.999 * intensity.clamp(g)) as u32;
-    let bbyte = (255.999 * intensity.clamp(b)) as u32;
-
-    texture[pixel_index] = rbyte as u8;
-    texture[pixel_index + 1] = gbyte as u8;
-    texture[pixel_index + 2] = bbyte as u8;*/
-
     let rgba_gamma = unsafe { f32x4_sqrt(pixel_color.0) };
 
     let lower_bound = unsafe { f32x4_splat(0.0) };
@@ -39,13 +24,7 @@ pub fn write_color(pixel_color: Vector3, texture: &mut Vec<u8>, pixel_index: usi
 
     let clamped = unsafe { f32x4_min(f32x4_max(rgba_gamma, lower_bound), upper_bound) };
 
-    // Experimental: Pack more colours in one vector
-    // u8x16 should fit 4 pixels as RGBA, and only use SIMD operations
-    // let color = unsafe { f32x4_nearest(f32x4_mul(f32x4_splat(255.999), clamped)) };
     let color = unsafe { f32x4_mul(f32x4_splat(255.999), clamped) };
-    
-    // console_log!("u8 lane 0 is {}", unsafe { u8x16_extract_lane::<0>(color) });
-
 
     texture[pixel_index] = unsafe { f32x4_extract_lane::<0>(color) } as u8;
     texture[pixel_index + 1] = unsafe { f32x4_extract_lane::<1>(color) } as u8;
